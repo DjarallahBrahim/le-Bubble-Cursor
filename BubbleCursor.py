628a22f949d95fec3ce736a9e012da0492b23984
DjarallahBrahim <djarallah.brahim@gmail.com>
2018-03-17 08:13:48 +0100

    adding some animation for the Target "zoomIn"

--- BubbleCursor.py
+++ BubbleCursor.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
+import time
 from math import *
 
 class BubbleCursor:
@@ -12,21 +13,36 @@ class BubbleCursor:
         self.x = 0
         self.y = 0
         self.size = 0
+        self.cercleCenterX = 0
+        self.cercleCenterY = 0
         self.cercleX = 0
         self.cercleY = 0
+        self.cercleSize = 0
         self.listTarget = listTargets
         self.closest = None
 
     def paint(self, qPainter):
         qPainter.setBrush(self.defaultCol)
-        #bbox = QRect(QPoint(self.x-(self.size/2), self.y-(self.size/2)), QSize(self.size, self.size))
-        bbox = QLine(self.x, self.y, self.cercleX, self.cercleY)
         pen = QPen()
-        pen.setWidth(3)
-        pen.setBrush(Qt.black)
+        #bbox = QRect(QPoint(self.x-(self.size/2), self.y-(self.size/2)), QSize(self.size, self.size))
+
+
+        bbox = QRect(QPoint(self.cercleCenterX-((self.cercleSize*3)/2), self.cercleCenterY-((self.cercleSize*3)/2)), QSize(self.cercleSize, self.cercleSize)*3)
+        pen.setWidth(0)
+        pen.setBrush(QColor(0, 0, 0))
+        qPainter.setPen(pen)
+        qPainter.setBrush(QColor("#e6005c"))
+        qPainter.drawEllipse(bbox)
+
+        #bbox = QRect(QPoint(self.cercleCenterX-((self.cercleSize*2.2)/2), self.cercleCenterY-((self.cercleSize*2.2)/2)), QSize(self.cercleSize, self.cercleSize)*2.2)
+        #qPainter.setBrush(QColor(102, 153, 153, 100))
+        #qPainter.drawEllipse(bbox)
+
+        bbox = QLine(self.x, self.y, self.cercleCenterX, self.cercleCenterY)
+        pen.setWidth(2)
+        pen.setBrush(QColor("#669999"))
         qPainter.setPen(pen)
         qPainter.drawLine(bbox)
-        #qPainter.drawEllipse(bbox)
 
     def move(self, x, y):
         self.x = x
@@ -45,8 +61,11 @@ class BubbleCursor:
             if(currDis == minDis):
                 self.closest = target
                 self.size = currDis
-                self.cercleX = (target.x+target.size/2)
-                self.cercleY = (target.y+target.size/2)
+                self.cercleCenterX = (target.x+target.size/2)
+                self.cercleCenterY = (target.y+target.size/2)
+                self.cercleX = (target.x)
+                self.cercleY = (target.y)
+                self.cercleSize = target.size
                 target.toSelect = True
                 break;
             else:
